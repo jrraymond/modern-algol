7181e89ed294b962a3747c26d8e41f1660a182ae
Justin Raymond <jraymond@wesleyan.edu>
2016-08-08 22:37:42 -0400

    llvm cg

--- codegen.c
+++ codegen.c
@@ -1,31 +1,72 @@
 #include "codegen.h"
 
+LLVMValueRef lvr_get(struct Array_lvr *vals, char *name)
+{
+  const char *v_name;
+  LLVMValueRef v;
+  for (Array_lvr_Itr i = array_lvr_begin(vals);
+      i != array_lvr_end(vals);
+      array_lvr_next(vals, &i)) {
+    v = array_lvr_get(vals, i);
+    v_name = LLVMGetValueName(v);
+    if (strcmp(v_name, name) == 0)
+      return v;
+  }
+  printf("function '%s' does not exist\n", name);
+  exit(1);
+}
 
-LLVMValueRef cgen_exp(LLVMBuilderRef b, struct maExp *e);
 void code_gen(char *name, struct maExp *e)
 {
+  struct Array_lvr vals;
+  array_lvr_init(&vals, 64);
+
   LLVMModuleRef mod = LLVMModuleCreateWithName(name);
 
-}
-LLVMValueRef cgen_tuple(LLVMBuilderRef b, struct maTuple *t)
-{
+  mk_pow(mod, &vals);
+
+  array_lvr_del(&vals);
 }
 
-LLVMValueRef cgen_prim_op(LLVMBuilderRef b, struct maPrimOp *op)
+void mk_pow(LLVMModuleRef m, struct Array_lvr *vals)
 {
-  //gen instructions for args
-  LLVMValueRef a = cgen_exp(b, op->arg);
+  LLVMTypeRef param_types[] = { LLVMDoubleType() };
+  LLVMTypeRef fn_type = LLVMFunctionType(LLVMDoubleType(), param_types, 2, false);
+  LLVMValueRef fn = LLVMAddFunction(m, "llvm.pow.f64", fn_type);
+  array_lvr_append(vals, fn);
+}
+
+
+LLVMValueRef cgen_prim_op(LLVMBuilderRef b, struct maPrimOp *op, struct Array_lvr *vals)
+{
+  //assume all primops are binary ops on a tuple
+  LLVMValueRef args[2];
+  args[0] = cgen_exp(b, op->arg->val.tuple.fst, vals);
+  args[1] = cgen_exp(b, op->arg->val.tuple.snd, vals);
+
   switch (op->tag) {
     case MA_PO_ADD:
-      break;
+      return LLVMBuildAdd(b, args[0], args[1], "add");
+    case MA_PO_SUB:
+      return LLVMBuildSub(b, args[0], args[1], "sub");
+    case MA_PO_MUL:
+      return LLVMBuildMul(b, args[0], args[1], "mul");
+    case MA_PO_DIV:
+      return LLVMBuildSDiv(b, args[0], args[1], "div");
+    case MA_PO_REM:
+      return LLVMBuildSRem(b, args[0], args[1], "rem");
+    case MA_PO_POW:;
+      //TODO cast from integer to fp
+      LLVMValueRef pow = lvr_get(vals, "llvm.pow.f64");
+      return LLVMBuildCall(b, pow, args, 2, "pow");
   }
 }
 
-LLVMValueRef cgen_exp(LLVMBuilderRef b, struct maExp *e)
+LLVMValueRef cgen_exp(LLVMBuilderRef b, struct maExp *e, struct Array_lvr *vals)
 {
   switch (e->tag) {
     case MA_EXP_PRIM_OP:
-      return cgen_prim_op(b, &e->val.op);
+      return cgen_prim_op(b, &e->val.op, vals);
     default:
       printf("UNIMPLIMENTED\n");
       exit(1);
--- codegen.h
+++ codegen.h
@@ -11,11 +11,25 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "types.h"
 #include "malgol.tab.h"
 
+#include "cutils/array.h"
+
+CUTILS_ARRAY(lvr, static inline, LLVMValueRef)
+
 void code_gen(char *name, struct maExp *e);
 
+void mk_pow(LLVMModuleRef m, struct Array_lvr *functions);
 
+LLVMValueRef cgen_exp(LLVMBuilderRef b, struct maExp *e, struct Array_lvr *vals);
+
+LLVMValueRef cgen_tuple(LLVMBuilderRef b, struct maTuple *t);
+
+LLVMValueRef cgen_prim_op(LLVMBuilderRef b, struct maPrimOp *op, struct Array_lvr *vals);
+
+LLVMValueRef lvr_get(struct Array_lvr *vals, char *name) ;
 #endif
