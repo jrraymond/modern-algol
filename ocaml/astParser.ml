fb884c5f2c5578ec2d790eea75d5c39fae3fd054
Justin Raymond <justin.raymond.14@gmail.com>
2017-03-11 13:08:41 -0500

    begin recursive descent parsing of expressions

--- ocaml/astParser.ml
+++ ocaml/astParser.ml
@@ -49,7 +49,42 @@ let build_typ =
   in go [];;
 
 
+(* The grammar for type is simple
+ * t ::= nat | cmd | (t) | t -> t
+ *)
 let parse_typ tkns = shunt_typ tkns |> build_typ;;
 
+(* We use recusive descent to parse expressions and commands.
+ * We require the grammar be non-left-recursive. The rule 'e ::= e(e)'
+ * violates that. 
+ *  e ::= d | d(e)
+ *  d ::= <int> | fix x:t is e | \x:t.e | cmd m
+ *)
+let rec parse_cmd tkns =
+  match tkns with
+  | "ret"::tkns' -> 
+      let e, tkns'' = parse_expe tkns' in
+      Ret e, tkns''
+  | _ -> raise (Failure "todo")
+and parse_expe tkns =
+  let e0, tkns' = parse_expd tkns in
+  if tkns' = []
+  then e0, tkns'
+  else
+    let e1, tkns'' = parse_expe tkns' in
+    App (e0, e1), tkns''
+and parse_expd tkns =
+  match tkns with
+  | [] -> raise (Failure "Unexpected end of input")
+  | "cmd"::tkns' ->
+      let m, tkns'' = parse_cmd tkns' in
+      Cmd m, tkns''
+  | t::tkns' ->
+      try
+        let i = int_of_string t in
+        Int i, tkns'
+      with Failure _ ->
+        Var t, tkns';;
 
-let parse tkns = Ret (Var "todo");;
+
+let parse tkns = parse_cmd tkns |> fst;;
