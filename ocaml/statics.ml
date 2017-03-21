23f6b6f81983a78de526a290a3ed473d82f61b73
Justin Raymond <justin.raymond.14@gmail.com>
2017-03-20 23:09:03 -0400

    setup statics tests

--- ocaml/statics.ml
+++ ocaml/statics.ml
@@ -1,4 +1,17 @@
 open Result;;
+open Ast;;
 
-let type_check exp = Ok exp;;
+
+
+
+let ctx_of_list xs = 
+  let ctx = Hashtbl.create 32 in
+  let () = List.iter (fun (v, t) -> Hashtbl.add ctx v t) xs in
+  ctx;;
+
+
+let string_of_ctx ctx = "";;
+
+
+let type_check ctx exp = Ok IntTyp;;
 
--- ocaml/statics_test.ml
+++ ocaml/statics_test.ml
@@ -8,13 +8,18 @@ let string_of_result err ok x =
   | Ok a -> ok a
   | Error a -> err a;;
 
-let string_of_exp_result =
-  string_of_result (fun s -> s) string_of_exp;;
+let string_of_typ_result =
+  string_of_result (fun s -> s) string_of_typ;;
 
-let well_typed_tests = List.map (fun (arg, ans) ->
-  let vs = string_of_exp arg in
-  let res = type_check arg in
-  let m = string_of_exp_result res ^ "<>" ^ string_of_exp_result ans in
+let well_typed_exp_tests = List.map (fun (arg0, arg1, ans) ->
+  let vs = string_of_ctx arg0 ^ "," ^ string_of_exp arg1 in
+  let res = type_check arg0 arg1 in
+  let m = string_of_typ_result res ^ "<>" ^ string_of_typ_result ans in
   vs >:: (fun _ -> assert_equal ~msg:m ans res))
-  [ 
+  [ (ctx_of_list [("x", IntTyp)], Var { label = "x"; index = 0 }, Ok IntTyp)
   ];;
+
+
+let () = run_test_tt_main ("statics" >:::
+  [ "exp" >::: well_typed_exp_tests
+  ]);;
