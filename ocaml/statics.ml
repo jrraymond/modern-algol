6be2eef8eb1bb7887a839979e02cf081103dfef8
Justin Raymond <justin.raymond.14@gmail.com>
2017-03-22 22:32:22 -0400

    statics tests pass

--- ocaml/statics.ml
+++ ocaml/statics.ml
@@ -4,63 +4,97 @@ open Ast;;
 
 
 
-let ctx_of_list xs = 
-  let ctx = Hashtbl.create 32 in
-  let () = List.iter (fun (v, t) -> Hashtbl.add ctx v t) xs in
-  ctx;;
+let ctx_of_list xs = xs;;
 
 let asg_of_list xs = 
-  let ctx = Hashtbl.create 32 in
-  let () = List.iter (fun (v, t) -> Hashtbl.add ctx v t) xs in
-  ctx;;
+  let asg = Hashtbl.create 32 in
+  let () = List.iter (fun (v, t) -> Hashtbl.add asg v t) xs in
+  asg;;
 
 
-let string_of_ctx ctx = "";;
+let string_of_ctx ctx = 
+  List.map string_of_typ ctx |>
+  Utils.intercalate ",";;
 
 
-let rec type_check_exp ctx asg = 
-  let rec tc_exp i e =
-    match e with
-    | Int i -> Ok IntTyp
-    | Var v ->
-        (try
-          Ok (Hashtbl.find ctx v.index)
-        with Not_found ->
-          Error v.label)
-    | Fix (x, t, e) ->
-        let () = Hashtbl.add ctx i t in
-        (match tc_exp (i + 1) e with
-        | Ok t0 ->
-            let () = Hashtbl.remove ctx i in
-            Ok t0
-        | e -> e)
-    | Abs (x, t, e0) ->
-        let () = Hashtbl.add ctx i t in
-        (match tc_exp (i + 1) e0 with
-        | Ok t0 ->
-            let () = Hashtbl.remove ctx i in
-            Ok (FunTyp (t, t0) )
-        | e -> e)
-    | App (e0, e1) ->
-        let r0 = tc_exp i e0 in
-        (match r0 with
-        | Ok (FunTyp (t0a, t0b)) ->
-            (match tc_exp i e1 with
-            | Ok t1 when t0a = t1 -> Ok t0b
-            | Ok t1 -> Error (string_of_typ t1 ^".")
-            | e -> e)
-        | Ok t0 -> Error ("expected function, found " ^ string_of_typ t0)
-        | e -> e)
-    | Cmd m -> type_check_cmd ctx asg i m
-  in tc_exp
-and type_check_cmd ctx asg =
-  let tc_cmd i m =
-    match m with
-    | Ret e ->
-        (match type_check_exp ctx asg i e with
+let string_of_asg asg = "";;
+
+
+let add_to_ctx ctx t = t::ctx;;
+
+
+let rec get_from_ctx v ctx i =
+  match ctx, i with
+  | [], _ -> Error (v ^ "not found")
+  | x::ctx', 0 -> Ok x
+  | x::ctx', i -> get_from_ctx v ctx' (i - 1);;
+
+
+let rem_from_ctx ctx =
+  match ctx with
+  | [] -> raise (Failure "empty context")
+  | _::ctx' -> ctx'
+
+
+let rec type_check_exp ctx asg e = 
+  match e with
+  | Int i -> Ok IntTyp
+  | Var v -> get_from_ctx v.label ctx v.index
+  | Fix (x, t, e0) ->
+      let ctx' = add_to_ctx ctx t in
+      type_check_exp ctx' asg e0
+  | Abs (x, t, e0) ->
+      let ctx' = add_to_ctx ctx t in
+      (match type_check_exp ctx' asg e0 with
+      | Ok t0 -> Ok (FunTyp (t, t0) )
+      | e -> e)
+  | App (e0, e1) ->
+      let r0 = type_check_exp ctx asg e0 in
+      (match r0 with
+      | Ok (FunTyp (t0a, t0b)) ->
+          (match type_check_exp ctx asg e1 with
+          | Ok t1 when t0a = t1 -> Ok t0b
+          | Ok t1 -> Error (string_of_typ t1 ^".")
+          | e -> e)
+      | Ok t0 -> Error ("expected function, found " ^ string_of_typ t0)
+      | e -> e)
+  | Cmd m ->
+      (match type_check_cmd ctx asg m with
+      | Ok _ -> Ok CmdTyp
+      | Error e -> Error e)
+and type_check_cmd ctx asg m =
+  match m with
+  | Ret e ->
+      (match type_check_exp ctx asg e with
+      | Ok IntTyp -> Ok CmdTyp
+      | Ok t -> Error (string_of_typ t)
+      | e -> e)
+  | Bnd (x, e, m0) ->
+      (match type_check_exp ctx asg e with
+      | Ok CmdTyp ->
+          let ctx' = add_to_ctx ctx IntTyp in
+          type_check_cmd ctx' asg m0
+      | Ok t -> Error ("expected Cmd, found " ^ string_of_typ t)
+      | e -> e)
+  | Dcl (x, e, m0) ->
+      (match type_check_exp ctx asg e with
+      | Ok IntTyp -> 
+          let () = Hashtbl.add asg x IntTyp in
+          (match type_check_cmd ctx asg m0 with
+          | Ok CmdTyp ->
+              let () = Hashtbl.remove asg x in
+              Ok CmdTyp
+          | e -> e)
+      | e -> e)
+  | Get x ->
+      if Hashtbl.mem asg x
+      then Ok CmdTyp
+      else Error "Assignable x not in scope"
+  | Set (x, e) ->
+      if Hashtbl.mem asg x
+      then 
+        (match type_check_exp ctx asg e with
         | Ok IntTyp -> Ok CmdTyp
-        | Ok t -> Error (string_of_typ t)
         | e -> e)
-    | _ -> Error "TODO"
-  in tc_cmd;;
+      else Error "Assignable x not in scope";;
 
--- ocaml/statics_test.ml
+++ ocaml/statics_test.ml
@@ -17,21 +17,33 @@ let string_of_typ_result =
   string_of_result (fun s -> s) string_of_typ;;
 
 
-let well_typed_exp_tests = List.map (fun (arg0, arg1, ans) ->
-  let vs = string_of_ctx arg0 ^ "," ^ string_of_exp arg1 in
+let well_typed_exp_tests = List.map (fun (ctx, exp, ans) ->
+  let vs = string_of_ctx ctx ^ "," ^ string_of_exp exp in
   let asg = asg_of_list [] in
-  let res = type_check_exp arg0 asg 0 arg1 in
+  let res = type_check_exp ctx asg exp in
   let m = string_of_typ_result res ^ "<>" ^ string_of_typ_result ans in
   vs >:: (fun _ -> assert_equal ~msg:m ans res))
-  [ (ctx_of_list [(0, IntTyp)], Var { label = "x"; index = 0 }, Ok IntTyp)
+  [ (ctx_of_list [IntTyp], Var { label = "x"; index = 0 }, Ok IntTyp)
   ; (ctx_of_list [], Int 0, Ok IntTyp)
   ; (ctx_of_list [], id_fun, Ok (FunTyp (IntTyp, IntTyp)))
   ; (ctx_of_list [], App (id_fun, Int 0), Ok IntTyp)
   ; (ctx_of_list [], Fix ("x", IntTyp, App (id_fun, Var { label = "x"; index = 0 })), Ok IntTyp)
+  ; (ctx_of_list [], Cmd (Ret (Int 0)), Ok CmdTyp)
   ];;
 
 
-let well_typed_cmd_tests = [];;
+let well_typed_cmd_tests = List.map (fun (asg, cmd, ans) ->
+  let vs = string_of_asg asg ^ "," ^ string_of_cmd cmd in
+  let ctx = ctx_of_list [] in
+  let res = type_check_cmd ctx asg cmd in
+  let m = string_of_typ_result res ^ " <> " ^ string_of_typ_result ans in
+  vs >:: (fun _ -> assert_equal ~msg:m ans res))
+  [ (asg_of_list [], Ret (Int 0), Ok CmdTyp)
+  ; (asg_of_list [], Bnd ("x", Cmd (Ret (Int 0)), Ret (Var { label = "x"; index = 0 })), Ok CmdTyp)
+  ; (asg_of_list [], Dcl ("a", Int 0, Get "a"), Ok CmdTyp)
+  ; (asg_of_list [("a", IntTyp)], Get "a", Ok CmdTyp)
+  ; (asg_of_list [("a", IntTyp)], Set ("a", Int 0), Ok CmdTyp)
+  ];;
 
 
 let () = run_test_tt_main ("statics" >:::
